3333b0498dd19188565b00f6fd284b295fa9b318
obaydullaa <cr7obaydul@gmail.com>
2022-08-02 14:21:57 +0600

    Handle submit, P1 & p2 Btn create and woork.

--- src/App.js
+++ src/App.js
@@ -1,54 +1,112 @@
 
-/**
- * 1. get input
- * 2. check this input valid or invalid
- * 3. 
- */
 import { useState } from "react";
 import ScoreCard from "./Components/ScoreCard";
 
-function App() {
 
-  const [inputText, setInputText] = useState(10); 
-  const [playerOn, setPlayerOn] = useState(0); 
-  const [playerTwo, setPlayerTwo] = useState(0); 
-  const [tractClass1, setTractClass1] = useState(false); 
-  const [tractClass2, setTractClass2] = useState(true); 
+
+function App() {
+ 
+// 1. submit click: 
+//      a. Get input value
+//      b. valid input input filed (optional)
+//      c. input value show Winning score 
+//      d. as Winning score variable
+//      e. hide input and submit btn 
+
+
+// 2. p1 click 
+//      b. Random generate
+//      a. P1 btn Disable, p2 btn Enable
+//      c. show random number in player p1 ui
+//      d. set as p1 variable
+//      e. Check if it matches the winning score
+//      f.    1. Alert pop up message = (Player one is won)
+//            2. Just ignore
+
+// 3. p2 click 
+//      b. Random generate
+//      a. P2 btn Disable, p1 btn Enable
+//      c. show random number in player p2 ui
+//      d. set as p1 variable
+//      e. Check if it matches the winning score
+//      f.    1. Alert pop up message = (Player Two is won)
+//            2. Just ignore
+
+// 4. Reset btn click:
+//      a. winning score zero
+//      b. enable p1  and p2 score zero
+//      c. show input and submit btn
+//      d. Enable p1 and p2 btn
+
+
+  const [winningScore, setWinningScore] = useState(10); 
+  const [winningScoreInput, setWinningScoreInput] = useState(''); 
+  const [isInputHide, setIsInputHide] = useState(false); 
+  const [playerOneValue, setPlayerOneValue] = useState(0); 
+  const [playerTwoValue, setplayerTwoValue] = useState(0); 
+  const [isPlayerOneBtnHide, setIsPlayerOneBtnHide] = useState(false); 
+  const [isPlayerTwoBtnHide, setIsPlayerTwoBtnHide] = useState(false); 
 
  
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-    setInputText(evt.target[0].value)
-    evt.target[0].value = '';
-  }
+const handleSubmit = (e) => {
+  e.preventDefault()
+  setWinningScore(winningScoreInput)
+  setIsInputHide(true)
+}
 
-  const playerBtnOn = () => {
-    setPlayerOn((prevValue) => prevValue + 1)
-    setTractClass1(true)
-  }
+const handleChange = (e) => {
+  // e.preventDefault();
+  setWinningScoreInput(parseInt(e.target.value))
+}
 
-  const playerBtnTwo = () => {
-    setPlayerTwo((prevValue) => prevValue + 1)
-    setTractClass2(false)
-  }
+const handleClickBtn = (e) => {
 
-  // const {inputText} = 
-  return (
+
+  if (e.target.innerHTML == 'Player 01') {
+    setPlayerOneValue(randomNumber(parseInt(winningScore)))
+    setIsPlayerOneBtnHide(true)
+    setIsPlayerTwoBtnHide(false)
     
+  } else if (e.target.innerHTML == 'Player 02') {
+    setplayerTwoValue(randomNumber(parseInt(winningScore)))
+    setIsPlayerTwoBtnHide(true)
+    setIsPlayerOneBtnHide(false)
+
+
+  } else {
+    console.log('reset')
+  }
+  
+}
+
+const randomNumber = (num) => {
+  const random = Math.floor(Math.random() * (num + 1));
+  return random;
+}
+
+if(winningScore === playerOneValue){
+  alert('winner player One')
+}
+
+if(winningScore === playerTwoValue){
+  alert('winner player Two')
+}
+
+  return (
     <div className='container'>
-      <ScoreCard inputText={inputText}/>
+      <ScoreCard winningScore={winningScore}/>
       <div className="players">
-        <h3>Player-On : {playerOn}</h3>
-        <h3>Player-Two: {playerTwo}</h3>
+        <h3>Player-One: {playerOneValue}</h3>
+        <h3>Player-Two: {playerTwoValue}</h3>
       </div>
         <form className="form-wrap" onSubmit={handleSubmit}>
-          <input required='required' type="number" placeholder="Enter Your Score Number"  id="input"/>
-          <button className="btn">Submit</button>
+          <input className={`${isInputHide ? 'hideinput' : ''}`} onChange={handleChange} value={winningScoreInput} required='required' type="number" placeholder="Enter Your Score Number"  id="input"/>
+          <button className={`btn ${isInputHide ? 'hideinput' : ''}`} type="submit" >Submit</button>
       </form>
       <div className="player-btn-wrap">
-        <button className={`btn player-btn ${playerOn ? 'disablepOn': ''}`} onClick={playerBtnOn}>Player 01</button>
-        <button className={`btn player-btn ${playerTwo ? 'disablepTwo': ''}`} onClick={playerBtnTwo}>Player 02</button>
-        <button className="btn reset-btn">Reset</button>
+        <button className={`btn player-btn ${isPlayerOneBtnHide ? 'disableBtn' : ''}`} onClick={handleClickBtn}>Player 01</button>
+        <button className={`btn player-btn ${isPlayerTwoBtnHide ? 'disableBtn' : ''}`} onClick={handleClickBtn}>Player 02</button>
+        <button className="btn reset-btn" onClick={handleClickBtn}>Reset</button>
       </div>
     </div>
   );
--- src/Components/ScoreCard.jsx
+++ src/Components/ScoreCard.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function ScoreCard({inputText}) {
+export default function ScoreCard({winningScore}) {
   return (
     <>
     <h2>Player Vs Player</h2>
     <div className='score'>
-            <h2>Winning score {inputText}</h2>
+            <h2>Winning score {winningScore}</h2>
     </div>
     </>
   )
